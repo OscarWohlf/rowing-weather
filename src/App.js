913f44baa8e843bcbe6b85c42e06d618ebbb43dd
oscarwohlfahrt <oscarjhw@gmail.com>
2024-08-18 12:50:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Rowing Weather App</h1>
       </header>
+      <main>
+        <p>Select a location to view the weather.</p>
+      </main>
     </div>
   );
 }
 
 export default App;
+
